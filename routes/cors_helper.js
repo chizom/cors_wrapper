9428980c6d57f6826a4ce49db841456eb15b3ce4
Chizom <chizomreal@gmail.com>
2023-05-18 11:51:59 +0100

    did some checking to see if requestParam was empty

--- routes/cors_helper.js
+++ routes/cors_helper.js
@@ -7,46 +7,79 @@ const jsonParser = bodyParser.json()
 const router = express.Router()
 
 
-
 router.post('/', jsonParser, (req, res, body) => {
-    // console.log(req.body)
-    const requestUrl = req.body.url
-    const requestMethod = req.body.method
-    const requestBodyParam = req.body.bodyParam
-    const requestHeaders = req.body.headers
-  //   const options = {
-  //     url: req.body.url,
-  //     method: req.body.method,
-  //     headers: {
-  //         'Accept': 'application/json',
-  //         'Accept-Charset': 'utf-8',
-  //         'User-Agent': 'my-reddit-client'
-  //     }
-  // };
-  axios({
-    url:requestUrl,
-    data:requestBodyParam,
-    method:requestMethod,
-    headers:requestHeaders,
-  })
-  .then(response => {
-    res.status(200).json(response.data);
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  });
+  const requestUrl = req.body.url;
+  const requestMethod = req.body.method;
+  const requestBodyParam = req.body.bodyParam;
+  const requestHeaders = req.body.headers;
+
+  let axiosConfig = {
+    url: requestUrl,
+    method: requestMethod,
+    headers: requestHeaders,
+  };
+
+  // Check if the requestBodyParam is not an empty object
+  if (Object.keys(requestBodyParam).length !== 0) {
+    axiosConfig.data = requestBodyParam;
+  }
+
+  axios(axiosConfig)
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+module.exports = router;
+
+
+
+// router.post('/', jsonParser, (req, res, body) => {
+  
+//     // console.log(req.body)
+//     const requestUrl = req.body.url
+//     const requestMethod = req.body.method
+//     const requestBodyParam = req.body.bodyParam
+//     const requestHeaders = req.body.headers
+//     // requestBodyParam.map(element => {
+//     //   console.log(element)
+//     // });
+//   //   const options = {
+//   //     url: req.body.url,
+//   //     method: req.body.method,
+//   //     headers: {
+//   //         'Accept': 'application/json',
+//   //         'Accept-Charset': 'utf-8',
+//   //         'User-Agent': 'my-reddit-client'
+//   //     }
+//   // };
+//   axios({
+//     url:requestUrl,
+//     data:requestBodyParam,
+//     method:requestMethod,
+//     headers:requestHeaders,
+//   })
+//   .then(response => {
+//     res.status(200).json(response.data);
+//   })
+//   .catch((err) => {
+//     res.status(500).json({ message: err });
+//   });
     
-  });
+//   });
 
 
-module.exports = router
+// module.exports = router
 
 
 
-// request({
-//   uri: requestUrl,
-//   method: requestMethod,
-//   json: requestBodyParam,
-// }).pipe(res.json({
-//     msg: 'email sent'
-// }));
+// // request({
+// //   uri: requestUrl,
+// //   method: requestMethod,
+// //   json: requestBodyParam,
+// // }).pipe(res.json({
+// //     msg: 'email sent'
+// // }));
